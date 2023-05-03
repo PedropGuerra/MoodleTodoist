688f99018c2059b67f9ac5e9108242712330e758
PedropGuerra <89150329+PedropGuerra@users.noreply.github.com>
2023-05-03 00:38:11 +0000

    Trocando para Playwright

--- app.py
+++ app.py
@@ -1,16 +1,39 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from time import sleep
+from playwright.sync_api import sync_playwright
 
+# Acesso Moodle
+LNK_LOGIN_MOODLE = "https://www.moodle.fsa.br/login/index.php"
+USERNAME = '751806'
+PASSWORD = '45038361889'
 
+# Base Links
+CURSOS_BASELINK = "course"
 
-navegador = webdriver.Firefox()
+with sync_playwright() as p:
+    browser = p.chromium.launch()
+    page = browser.new_page()
 
-navegador.get("https://www.moodle.fsa.br/login/index.php")
+    # Acesso ao Moodle
+    page.goto(LNK_LOGIN_MOODLE)
 
-navegador.find_elements(By.XPATH,'//*[@id="username"]')
+    username_field = page.locator('//*[@id="username"]')
+    username_field.type(USERNAME)
 
+    password_field = page.locator('//*[@id="password"]')
+    password_field.type(PASSWORD)
+    password_field.press("Enter")
 
+    # Validaçõa de Acesso
+    if page.title() == "Painel":
+        print("Login Efetuado com sucesso")
 
-navegador.quit()
+    else:
+        print("Login Mal Sucedido")
+
+    # Listar todos os cursos do Moodle
+    cursos = page.locator('a')
+    # set(cursos)
+
+    print(cursos.get_attribute('name'))
+
+    browser.close()
